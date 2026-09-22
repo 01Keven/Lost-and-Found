9a263de3674c47816441bb1678bc42e8cc8ff68f
keven01 <kevenchristiancode@gmail.com>
2026-09-22 17:36:26 -0300

    feat: Implement CycleManager functionality with NPC handling and input actions

--- Assets/Scripts/CycleManager/CycleManager.cs
+++ Assets/Scripts/CycleManager/CycleManager.cs
@@ -1,16 +1,96 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CycleManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [System.Serializable]
+    public struct CycleData
     {
-        
+        public string cycleName;
+        public int npcCount;
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<CycleData> cycles;
+    private int currentCycleIndex = 0;
+    private int currentNpcIndex = 0;
+    private bool isCycleActive = false;
+
+    public InputActionReference nextNpcAction;
+
+    private void Onable()
     {
-        
+        if (nextNpcAction != null)
+        {
+            nextNpcAction.action.Disable();
+            nextNpcAction.action.performed -= OnNextNpcPerfomed;
+        }     
+    }
+
+    public void StartCycle()
+    {
+        if (isActiveAndEnabled)
+        {
+            Debug.Log("Fila começando");
+            return;
+        }
+
+        if (currentCycleIndex >= cycles.Count)
+        {
+            Debug.Log("todos os ciclos encerraram");
+            return;
+        }
+
+        isCycleActive = true;
+        currentNpcIndex = 0;
+        CycleData currentCycle = cycles[currentCycleIndex];
+
+        Debug.Log($"DEBUG: === INICIANDO CICLO: {currentCycle.cycleName.ToUpper()} ===");
+        Debug.Log($"DEBUG: Fila criada com {currentCycle.npcCount} NPCs.");
+
+        SpawnNextNPC();
+
+    }
+
+    private void SpawnNextNPC()
+    {
+        CycleData currentCycle = cycles[currentCycleIndex];
+
+        if (currentCycleIndex < currentCycle.npcCount)
+        {
+            currentCycleIndex++;
+            Debug.Log($"DEBUG: [NPC CHEGOU] Atendendo NPC {currentNpcIndex} de {currentCycle.npcCount} (Ciclo: {currentCycle.cycleName}).");
+        }
+        else
+        {
+            EndCycle();
+        }
+
+
+    }
+
+    private void OnNextNpcPerfomed(InputAction.CallbackContext context)
+    {
+        if (!isCycleActive)
+        {
+            Debug.Log("DEBUG: Input 'nextNpc' pressionado. Dispensando NPC atual...");
+            SpawnNextNPC();
+        }
+    }
+
+    private void EndCycle()
+    {
+        isCycleActive = false;
+
+        currentCycleIndex++;
+
+        if (currentCycleIndex >= cycles.Count)
+        {
+            Debug.Log("DEBUG: O shopping fechou. Fim do dia!");
+        }
+        else
+        {
+            Debug.Log($"DEBUG: Clique no objeto 3D para iniciar o próximo ciclo ({cycles[currentCycleIndex].cycleName}).");
+        }
     }
 }
